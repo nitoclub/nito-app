7b740ed6d3bfaea71dc3bde307715e589e4c73d9
Ryo Takeuchi <develop@r09i.me>
2023-10-29 12:23:06 +0900

    :sparkles: edge-to-edge 対応

--- app/android/src/main/java/club/nito/android/MainActivity.kt
+++ app/android/src/main/java/club/nito/android/MainActivity.kt
@@ -2,12 +2,16 @@ package club.nito.android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import club.nito.core.designsystem.theme.NitoTheme
 
@@ -15,6 +19,18 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.Transparent.toArgb(),
+                darkScrim = Color.Transparent.toArgb(),
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.Transparent.toArgb(),
+                darkScrim = Color.Transparent.toArgb(),
+            ),
+        )
+        window.isNavigationBarContrastEnforced = false
 
         setContent {
             val windowSize = calculateWindowSizeClass(this)
