982bfbe92b9f6e2a030c9a407a0a619c2b086d14
Ryo Takeuchi <develop@r09i.me>
2023-11-04 15:33:55 +0900

    :sparkles: スケジュールのモデルを変更

--- core/model/src/commonMain/kotlin/club/nito/core/model/Schedule.kt
+++ core/model/src/commonMain/kotlin/club/nito/core/model/Schedule.kt
@@ -5,9 +5,17 @@ import kotlinx.datetime.Instant
 /**
  * スケジュール
  * @param id ID
- * @param scheduledAt 予定日
+ * @param scheduledAt 予定日時
+ * @param metAt 集合日時
+ * @param venueId 開催場所ID
+ * @param meetId 集合場所ID
+ * @param description 説明文
  */
 data class Schedule(
     val id: String,
     val scheduledAt: Instant,
+    val metAt: Instant,
+    val venueId: String,
+    val meetId: String,
+    val description: String,
 )
--- core/network/src/commonMain/kotlin/club/nito/core/network/schedule/model/NetworkSchedule.kt
+++ core/network/src/commonMain/kotlin/club/nito/core/network/schedule/model/NetworkSchedule.kt
@@ -9,17 +9,33 @@ import kotlinx.serialization.Serializable
 internal data class NetworkSchedule(
     val id: String,
     val scheduledAt: Instant,
+    val metAt: Instant,
+    val venueId: String,
+    val meetId: String,
+    val description: String,
 ) {
     fun toSchedule() = Schedule(
         id = id,
         scheduledAt = scheduledAt,
+        metAt = metAt,
+        venueId = venueId,
+        meetId = meetId,
+        description = description,
     )
 }
 
 internal fun createFakeNetworkSchedule(
     id: String = "bbe00d24-d840-460d-a127-f23f9e472cc6",
     scheduledAt: Instant = Clock.System.now(),
+    metAt: Instant = Clock.System.now(),
+    venueId: String = "bbe00d24-d840-460d-a127-f23f9e472cc6",
+    meetId: String = "bbe00d24-d840-460d-a127-f23f9e472cc6",
+    description: String = "いつも通り 11:00 から 90 分で入る予定です。\n10:30 にレイクタウンの kaze で集合して向かいます。",
 ) = NetworkSchedule(
     id = id,
     scheduledAt = scheduledAt,
+    metAt = metAt,
+    venueId = venueId,
+    meetId = meetId,
+    description = description,
 )
