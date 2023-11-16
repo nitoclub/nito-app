c7f604fd0997192124e81ba8ad37e2be32478bb7
Ryo Takeuchi <develop@tatsutakein.jp>
2023-11-16 10:14:13 +0900

    :+1: デバッグ時のログを導入

--- core/data/src/commonMain/kotlin/club/nito/core/data/OfflineFirstScheduleRepository.kt
+++ core/data/src/commonMain/kotlin/club/nito/core/data/OfflineFirstScheduleRepository.kt
@@ -2,19 +2,24 @@ package club.nito.core.data
 
 import club.nito.core.model.Schedule
 import club.nito.core.network.schedule.ScheduleRemoteDataSource
+import co.touchlab.kermit.Logger
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class OfflineFirstScheduleRepository(
     private val remoteDataSource: ScheduleRemoteDataSource,
 ) : ScheduleRepository {
+    private val log = Logger.withTag("OfflineFirstScheduleRepository")
+
     override val scheduleListFlow: Flow<List<Schedule>> = flow {
         // TODO: LocalDataSource
         emit(remoteDataSource.getScheduleList(limit = 10))
     }
 
     override suspend fun getScheduleList(limit: Int): List<Schedule> =
-        remoteDataSource.getScheduleList(limit = limit)
+        remoteDataSource.getScheduleList(limit = limit).also {
+            log.d { "getScheduleList: $it" }
+        }
 
     override fun scheduleFlow(id: String): Flow<Schedule> = flow {
         // TODO: LocalDataSource
--- core/network/src/commonMain/kotlin/club/nito/core/network/schedule/SupabaseScheduleRemoteDataSource.kt
+++ core/network/src/commonMain/kotlin/club/nito/core/network/schedule/SupabaseScheduleRemoteDataSource.kt
@@ -2,6 +2,7 @@ package club.nito.core.network.schedule
 
 import club.nito.core.model.Schedule
 import club.nito.core.network.schedule.model.NetworkSchedule
+import co.touchlab.kermit.Logger
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.postgrest
 import io.github.jan.supabase.postgrest.query.Order
@@ -9,6 +10,7 @@ import io.github.jan.supabase.postgrest.query.Order
 class SupabaseScheduleRemoteDataSource(
     private val client: SupabaseClient,
 ) : ScheduleRemoteDataSource {
+    private val log = Logger.withTag("SupabaseScheduleRemoteDataSource")
     private val postgrest = client.postgrest["schedules"]
 
     override suspend fun getScheduleList(limit: Int): List<Schedule> = postgrest
@@ -20,6 +22,7 @@ class SupabaseScheduleRemoteDataSource(
         )
         .decodeList<NetworkSchedule>()
         .map(NetworkSchedule::toSchedule)
+        .also { log.d { "getScheduleList: $it" } }
 
     override suspend fun getSchedule(id: String): Schedule {
         return postgrest
