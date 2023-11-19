0eb583a80e1a180493de728c48a14181669e1e00
Ryo Takeuchi <develop@tatsutakein.jp>
2023-11-19 11:20:30 +0900

    :bookmark: v0.2.1

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
     }
     signingConfigs {
         create("dev") {
