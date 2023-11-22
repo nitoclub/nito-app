ae29cd8cb263845f743e6ed3fe216436f0ad2931
Ryo Takeuchi <develop@tatsutakein.jp>
2023-11-22 22:18:58 +0900

    :sparkles: ViewModel を継承

--- core/ui/src/commonMain/kotlin/club/nito/core/ui/StateMachine.kt
+++ core/ui/src/commonMain/kotlin/club/nito/core/ui/StateMachine.kt
@@ -3,10 +3,11 @@ package club.nito.core.ui
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import moe.tlaster.precompose.viewmodel.ViewModel
 
 /**
  * [StateMachine]
  */
-public open class StateMachine {
+public open class StateMachine: ViewModel() {
     public val stateMachineScope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 }
