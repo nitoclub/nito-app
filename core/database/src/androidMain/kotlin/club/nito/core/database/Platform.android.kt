57bc2b7712781f3bad4a9589463aa52b49e3609a
Ryo Takeuchi <develop@tatsutakein.jp>
2023-11-23 11:11:38 +0900

    :recycle: database モジュールに explicitApi を追加

--- core/database/src/androidMain/kotlin/club/nito/core/database/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package club.nito.core.database
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
