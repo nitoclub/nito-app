e76e96c51484b1ceed8356d62b43849922698a85
Ryo Takeuchi <develop@tatsutakein.jp>
2023-12-05 23:12:22 +0900

    :bookmark: v0.4.0

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -17,8 +17,8 @@ val keystoreExits = keystorePropertiesFile.exists()
 android {
     namespace = "club.nito.app"
     defaultConfig {
-        versionCode = 7
-        versionName = "0.3.3"
+        versionCode = 8
+        versionName = "0.4.0"
     }
     signingConfigs {
         create("dev") {
