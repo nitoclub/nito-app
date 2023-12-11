31ef0caadb90de3f8d2c55cdaeea35a4482a79ae
Ryo Takeuchi <develop@tatsutakein.jp>
2023-12-11 18:59:06 +0900

    :sparkles: NitoCoroutineDispatchers を追加

--- app/shared/src/commonMain/kotlin/club/nito/app/shared/di/AppModule.kt
+++ app/shared/src/commonMain/kotlin/club/nito/app/shared/di/AppModule.kt
@@ -1,12 +1,26 @@
 package club.nito.app.shared.di
 
 import club.nito.app.shared.NitoAppStateMachine
+import club.nito.core.common.NitoCoroutineDispatchers
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.IO
 import org.koin.dsl.module
 
 val appModule = module {
     factory {
         NitoAppStateMachine(
             authStatusStream = get(),
+        )
+    }
+    single {
+        @OptIn(ExperimentalCoroutinesApi::class)
+        NitoCoroutineDispatchers(
+            io = Dispatchers.IO,
+            databaseWrite = Dispatchers.IO.limitedParallelism(1),
+            databaseRead = Dispatchers.IO.limitedParallelism(4),
+            computation = Dispatchers.Default,
+            main = Dispatchers.Main,
         )
     }
 }
