3a2eeb82a39b898da41b250900a4275d36d0061e
Ryo Takeuchi <develop@tatsutakein.jp>
2024-01-02 22:33:36 +0900

    :sparkles: DefaultParticipantRepository に ParticipantDao を追加

--- core/data/src/commonMain/kotlin/club/nito/core/data/DefaultParticipantRepository.kt
+++ core/data/src/commonMain/kotlin/club/nito/core/data/DefaultParticipantRepository.kt
@@ -1,19 +1,30 @@
 package club.nito.core.data
 
+import club.nito.core.database.participant.ParticipantDao
 import club.nito.core.model.participant.Participant
 import club.nito.core.model.participant.ParticipantDeclaration
 import club.nito.core.model.participant.ParticipantStatus
+import club.nito.core.model.participant.ParticipantUser
 import club.nito.core.model.schedule.ScheduleId
 import club.nito.core.network.participation.ParticipantRemoteDataSource
+import kotlinx.coroutines.flow.Flow
 
 public class DefaultParticipantRepository(
     private val remoteDataSource: ParticipantRemoteDataSource,
+    private val dao: ParticipantDao,
 ) : ParticipantRepository {
+    override fun participantUsersStream(scheduleId: String): Flow<List<ParticipantUser>> =
+        dao.participantUsersStream(scheduleId = scheduleId)
+
     override suspend fun getParticipants(scheduleId: String): List<Participant> =
-        remoteDataSource.getParticipants(scheduleId = scheduleId)
+        remoteDataSource.getParticipants(scheduleId = scheduleId).also {
+            dao.upsert(entities = it)
+        }
 
     override suspend fun getParticipants(scheduleIds: List<String>): List<Participant> =
-        remoteDataSource.getParticipants(scheduleIds = scheduleIds)
+        remoteDataSource.getParticipants(scheduleIds = scheduleIds).also {
+            dao.upsert(entities = it)
+        }
 
     override suspend fun existParticipantByUserId(scheduleId: ScheduleId, userId: String): Boolean =
         remoteDataSource.existParticipantByUserId(scheduleId = scheduleId, userId = userId)
@@ -21,9 +32,15 @@ public class DefaultParticipantRepository(
     override suspend fun fetchParticipantStatus(scheduleId: ScheduleId, userId: String): ParticipantStatus =
         remoteDataSource.fetchParticipantStatus(scheduleId = scheduleId, userId = userId)
 
-    override suspend fun insertParticipate(declaration: ParticipantDeclaration): Participant =
-        remoteDataSource.insertParticipate(declaration = declaration)
+    override suspend fun insertParticipate(declaration: ParticipantDeclaration): Participant {
+        val participant = remoteDataSource.insertParticipate(declaration = declaration)
+        dao.upsert(participant)
+        return participant
+    }
 
-    override suspend fun updateParticipate(declaration: ParticipantDeclaration): Participant =
-        remoteDataSource.updateParticipate(declaration = declaration)
+    override suspend fun updateParticipate(declaration: ParticipantDeclaration): Participant {
+        val participant = remoteDataSource.updateParticipate(declaration = declaration)
+        dao.upsert(participant)
+        return participant
+    }
 }
