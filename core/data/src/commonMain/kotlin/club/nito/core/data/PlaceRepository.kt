f2aaa07f558f6923d68abea50801b626180c5214
Ryo Takeuchi <develop@tatsutakein.jp>
2024-01-02 23:27:13 +0900

    :sparkles: DefaultPlaceRepository に PlaceDao を追加

--- core/data/src/commonMain/kotlin/club/nito/core/data/PlaceRepository.kt
+++ core/data/src/commonMain/kotlin/club/nito/core/data/PlaceRepository.kt
@@ -2,11 +2,18 @@ package club.nito.core.data
 
 import club.nito.core.model.place.Place
 import club.nito.core.model.place.PlaceId
+import kotlinx.coroutines.flow.Flow
 
 /**
  * 場所に関するリポジトリ
  */
 public sealed interface PlaceRepository {
+    /**
+     * 場所のストリームを取得する
+     * @param placeId 取得する場所のID
+     */
+    public fun placeStream(placeId: PlaceId): Flow<Place?>
+
     /**
      * 場所を取得する
      * @param ids 取得する場所のID
