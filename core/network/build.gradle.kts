b58f5361b2230150470712c017f006a74187c8b1
Ryo Takeuchi <develop@tatsutakein.jp>
2024-03-09 22:28:31 +0900

    chore(deps): apollographql への依存を追加

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,9 +1,12 @@
+import org.jetbrains.compose.internal.utils.getLocalProperty
+
 plugins {
     id("nito.primitive.kmp")
     id("nito.primitive.kmp.android")
     id("nito.primitive.kmp.ios")
     id("nito.primitive.detekt")
     id("nito.primitive.kmp.serialization")
+    alias(libs.plugins.apollographql)
 }
 
 android.namespace = "club.nito.core.network"
@@ -27,6 +30,9 @@ kotlin {
                 implementation(libs.ktorKotlinxSerialization)
                 implementation(libs.ktorClientContentNegotiation)
 
+                implementation(libs.apolloRuntime)
+                implementation(libs.apolloAdapters)
+
                 implementation(libs.supabaseGotrue)
                 implementation(libs.supabasePostgrest)
                 implementation(libs.supabaseRealtime)
@@ -47,6 +53,33 @@ kotlin {
         iosMain {
             dependencies {
                 implementation(libs.ktorClientDarwin)
+            }
+        }
+    }
+}
+
+apollo {
+    service("api") {
+        // GraphQL configuration here.
+        // https://www.apollographql.com/docs/kotlin/advanced/plugin-configuration/
+        packageName.set("club.nito.core.network.graphql")
+
+        // https://www.apollographql.com/docs/kotlin/essentials/custom-scalars/#in-buildgradlekts
+        mapScalarToKotlinString("uuid")
+        mapScalar("timestamptz", "kotlinx.datetime.Instant", "com.apollographql.apollo3.adapter.KotlinxInstantAdapter")
+
+        // This will create a downloadStarwarsApolloSchemaFromIntrospection task
+        val accessToken = getLocalProperty(key = "nito.hasura.accessToken")
+        accessToken?.let {
+            introspection {
+                endpointUrl.set("https://nito-dev.hasura.app/v1/graphql")
+                headers.set(
+                    mapOf(
+                        "Authorization" to "Bearer $it",
+                    ),
+                )
+                // The path is interpreted relative to the current project here, no need to prepend 'app'
+                schemaFile.set(file("src/commonMain/graphql/schema.graphqls"))
             }
         }
     }
