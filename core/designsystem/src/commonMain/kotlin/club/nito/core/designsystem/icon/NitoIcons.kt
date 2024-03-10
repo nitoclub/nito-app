0eff07dafc59e43ca987a612b650db6ab8485448
Ryo Takeuchi <develop@tatsutakein.jp>
2024-03-10 09:38:44 +0900

    feat: プロフィール画像が設定されていない場合のデフォルトアイコンを追加

--- core/designsystem/src/commonMain/kotlin/club/nito/core/designsystem/icon/NitoIcons.kt
+++ core/designsystem/src/commonMain/kotlin/club/nito/core/designsystem/icon/NitoIcons.kt
@@ -1,11 +1,13 @@
 package club.nito.core.designsystem.icon
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.ui.graphics.vector.ImageVector
 
 public object NitoIcons {
+    public val AccountCircle: ImageVector = Icons.Filled.AccountCircle
     public val Visibility: ImageVector = Icons.Filled.Visibility
     public val VisibilityOff: ImageVector = Icons.Filled.VisibilityOff
 }
